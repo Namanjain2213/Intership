c45a5d803a36bfa39551f8a55c17bfca6660b506
Namanjain2213 <nj626433@gmail.com>
2025-01-09 17:41:38 +0530

    new

--- firstbench/src/components/CompareAccuracyBoxes.jsx
+++ firstbench/src/components/CompareAccuracyBoxes.jsx
@@ -3,14 +3,14 @@ import { ChevronRight } from 'react-feather';
 
 export function CompareAccuracyBoxes() {
     return (
-        <div className='grid lg:grid-cols-3 sm:gap-2 md:grid-cols-2 sm:grid-cols-1 w-full justify-center items-center'>
-            <div className="bg-white h-[250px] w-[250px] sm:w-[300px] sm:h-[300px] rounded-lg shadow p-6">
+        <div className='grid lg:grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1 w-full justify-center items-center'>
+            <div className="bg-white md:h-[250px] md:w-[250px] sm:w-[300px] sm:h-[300px] rounded-lg shadow p-6">
                 <h3 className="text-lg font-semibold mb-4">Compare Accuracy</h3>
             </div>
-            <div className="bg-white rounded-lg h-[250px] w-[250px] shadow p-6">
+            <div className="bg-white rounded-lg md:h-[250px] md:w-[250px] sm:w-[300px] sm:h-[300px] shadow p-6">
                 <h3 className="text-lg font-semibold mb-4">Compare Accuracy</h3>
             </div>
-            <div className="bg-white rounded-lg h-[250px] w-[250px] shadow p-6">
+            <div className="bg-white rounded-lg md:h-[250px] md:w-[250px] sm:w-[300px] sm:h-[300px] shadow p-6">
                 <h3 className="text-lg font-semibold mb-4">Compare Accuracy</h3>
             </div>
         </div>
